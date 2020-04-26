b976a4536ac9c558ede1d3fbff7721d0ea4ad7e8
Me <alicealmeyda72@gmail.com>
2020-04-26 12:24:17 -0300

    Criando variáveis e como declarar uma variável em python de maneira correta

---
+++ pythonVariables/creating_variables.py
@@ -0,0 +1,25 @@
+x = 5
+y = "Alice"
+
+print(x)
+print(y)
+
+a = 3 # a is of type int
+a = "Olá" # a is of type str
+
+# variables in python are case-sensitive
+
+
+#Legal variables
+myvar = "Alice"
+my_var = "Alice"
+_my_var = "Alice"
+myVar = "Alice"
+MYVAR = "Alice"
+myvar2 = "Alice"
+
+
+#Illegal Variables
+#2myvar = "Alice"
+#my-var = "Alice"
+#my var = "Alice"
