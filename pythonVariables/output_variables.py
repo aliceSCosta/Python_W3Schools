481ae569d815455ea4f9f00d200632d15942fc5d
Me <alicealmeyda72@gmail.com>
2020-04-26 12:23:16 -0300

    Saída de dados em python

---
+++ pythonVariables/output_variables.py
@@ -0,0 +1,18 @@
+x = "awesome"
+
+print("Python is " + x)
+
+#you can also use the + character to add a variable to another variable
+
+x = "Python is " 
+y = "awesome"
+z = x + y
+
+print(z)
+
+#for numbers, the + character works as a mathematical operator
+
+x = 3
+y = 8
+z = x + y
+print(z)
