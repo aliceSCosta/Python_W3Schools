d0ca08ace612a425b7e71669cc8535d1f4a0e374
Me <alicealmeyda72@gmail.com>
2020-04-29 13:09:47 -0300

    Configurando tipos de dados em uma variável.

---
+++ pythonDataTypes/settingDataType.py
@@ -0,0 +1,71 @@
+#Para especificar um tipo de dado basta defini-lo antes do valor da variável e colocar o valor entre parenteses
+
+# String (str)
+x = "Hello World"
+print(x)
+print(type(x))
+
+#Int
+x = 20
+print(x)
+print(type(x))
+
+#Float
+x = 20.5
+print(x)
+print(type(x))
+
+#Complex
+x = 1j
+print(x)
+print(type(x))
+
+#List
+x = ['Arroz', 'Feijão', 'Batata', 'Macarrão']
+print(x)
+print(type(x))
+
+#Tuple
+x  = ('Maçã, Banana, Cereja')
+print(x)
+print(type(x))
+
+#Range
+x = range (6)
+print(x)
+print(type(x))
+
+#Dict
+x = {'name': 'Alice', 'age': 16}
+print(x)
+print(type(x))
+
+#Set
+x = {'Alice', 'Alana', 'Amanda'}
+print(x)
+print(type(x))
+
+#Frozenset
+x = frozenset({'água', 'suco', 'refri'})
+print(x)
+print(type(x))
+
+#Bool
+x = True
+print(x)
+print(type(x))
+
+#Bytes
+x = b"Hello"
+print(x)
+print(type(x))
+
+#Bytearray
+x = bytearray(5)
+print(x)
+print(type(x))
+
+#Memoryview
+x = memoryview(bytes(5))
+print(x)
+print(type(x))
