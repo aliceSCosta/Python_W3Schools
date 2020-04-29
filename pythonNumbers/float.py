4dd1969ee918413f49aae30f585aaccf67078760
Me <alicealmeyda72@gmail.com>
2020-04-29 13:11:17 -0300

    Tipo de número float.

---
+++ pythonNumbers/float.py
@@ -0,0 +1,17 @@
+x = 1554.0546584
+y = 1.56
+z = 1.8
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Float também pode ser um número científico com um "e" para indicar a potência de 10.
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
