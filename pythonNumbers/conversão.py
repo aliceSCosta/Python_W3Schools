e85e117f6dad4958bf3689d876d43329936bdedd
Me <alicealmeyda72@gmail.com>
2020-04-29 13:10:16 -0300

    Como converter tipos de dados.

---
+++ pythonNumbers/conversão.py
@@ -0,0 +1,22 @@
+#Você pode converter de um tipo para outro com os métodos int(), float()e complex()
+
+x = 1 # int
+y = 2.8 # float
+z = 1j # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
